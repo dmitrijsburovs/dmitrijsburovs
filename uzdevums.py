24317cbedf295ab75f3237378d0f21a3008f1795
dmitrijsburovs <90191218+dmitrijsburovs@users.noreply.github.com>
2021-11-16 13:19:01 +0000

    d

---
+++ uzdevums.py
@@ -0,0 +1,23 @@
+class Rekins:
+
+  def __init__ (self,klients,veltijums,izmers,materials):
+  
+    self.klients = klients
+    self.veltijums = veltijums
+    self.izmers = izmers
+
+#print(Rekins)
+
+def izdruka (self):
+  print(f"Klienta vards{self.klients}")
+  print(f"Klienta vards{self.veltijums}")
+  print(f"Klienta vards{self.izmers}")
+  print(f"Klienta vards{self.materials}")
+
+def aprekins (self):
+  darba_samaksa = 15
+  PVN = 21
+produkta_cena = (veltījuma teksta garums simbolos) * 1.2 +
+(platums/100 * garums/100 * augstums/100)/ 3 * materiala_cena
+PVN_summa = (produkta_cena + darba_samaksa)*PVN/100
+rekina_summa = (produkta_cena + darba_samaksa) + PVN_summa
