118da61dc0481df44f870b749b7e282e90885c1d
dmitrijsburovs <90191218+dmitrijsburovs@users.noreply.github.com>
2022-01-11 21:36:41 +0000

    g

--- 10.01/uzdevums11.01.py
+++ 10.01/uzdevums11.01.py
@@ -1,14 +1,14 @@
 import requests
 from bs4 import BeautifulSoup
 
-lapa = requests.get('https://vvsprogramm.github.io/2021_debug/')
-print(lapa)
-print(lapa.content)
+lapa = requests.get('https://vvsprogramm.github.io/A/')
+# print(lapa)
+# print(lapa.content)
 
 soup = BeautifulSoup(lapa.content, 'html.parser')
-print(soup.prettify())
+# print(soup.prettify())
 print(list(soup.children))
-print([type(item) for item in list(soup.children)])
+# print([type(item) for item in list(soup.children)])
 
 html = list(soup.children)[2]
 print(list(html.children))
@@ -16,5 +16,5 @@ print(list(html.children))
 body = list(html.children)[3]
 print(list(body.children))
 
-p = list(body.children)[1]
-print(p.get_text())
+# p = list(body.children)[1]
+# print(p.get_text())
